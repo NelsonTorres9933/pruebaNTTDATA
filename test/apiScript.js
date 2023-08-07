9bcd324848e12cd4a619107fe061745454d2f81c
Nelson Torres <nelson.perez@cscgfm.com>
2023-08-07 05:47:08 -0500

    finish APIs tests

--- test/apiScript.js
+++ test/apiScript.js
@@ -8,29 +8,29 @@ const petsInfo = require("../fixtures/petsSold.json");
 let petsSold = [];
 
 describe("Data processing in APIs", function () {
-  //it("Create user", async function () {
-  //  request(endpoint)
-  //    .post("user/createWithArray")
-  //    .send(userInfo)
-  //    .set("Accept", "application/json")
-  //    .expect(200)
-  //    .then((res) => {
-  //      console.log(res.body);
-  //    });
-  //});
-  //
-  //it("Retrieve user info", async function () {
-  //  request(endpoint)
-  //    .get("user/" + userInfo[0].username)
-  //    .expect(200)
-  //    .then((res) => {
-  //      let data = JSON.stringify(res.body);
-  //      fs.writeFile("fixtures/userInfoCreated.json", data, function (err) {
-  //        if (err) throw err;
-  //      });
-  //      console.log(res.body);
-  //    });
-  //});
+  it("Create user", async function () {
+    request(endpoint)
+      .post("user/createWithArray")
+      .send(userInfo)
+      .set("Accept", "application/json")
+      .expect(200)
+      .then((res) => {
+        console.log(res.body);
+      });
+  });
+
+  it("Retrieve user info", async function () {
+    request(endpoint)
+      .get("user/" + userInfo[0].username)
+      .expect(200)
+      .then((res) => {
+        let data = JSON.stringify(res.body);
+        fs.writeFile("fixtures/userInfoCreated.json", data, function (err) {
+          if (err) throw err;
+        });
+        console.log(res.body);
+      });
+  });
 
   it("Names of pets that have been sold", async function () {
     request(endpoint)
